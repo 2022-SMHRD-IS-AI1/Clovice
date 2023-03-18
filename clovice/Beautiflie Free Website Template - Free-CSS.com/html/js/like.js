8f92b8a5ede9dc844e1336555c3b78c6f78201d3
sebin <choak5802@gmail.com>
2023-03-18 20:16:09 +0900

    여러가지 수정

--- clovice/Beautiflie Free Website Template - Free-CSS.com/html/js/like.js
+++ clovice/Beautiflie Free Website Template - Free-CSS.com/html/js/like.js
@@ -1,12 +1,12 @@
 const btnLikes = document.querySelectorAll(".btn_like");
 
-        btnLikes.forEach((btnlike) => {
-            btnlike.addEventListener("click", function() {
-                if($(this).hasClass('btn_unlike')){
-                    $(this).removeClass('btn_unlike');
-                  }
-                  else{
-                    $(this).addClass('btn_unlike');
-                  }
-            });
-        });
+btnLikes.forEach((btnlike) => {
+  btnlike.addEventListener("click", function () {
+    if ($(this).hasClass('btn_unlike')) {
+      $(this).removeClass('btn_unlike');
+    }
+    else {
+      $(this).addClass('btn_unlike');
+    }
+  });
+});
